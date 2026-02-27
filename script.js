fbcebf69881ddec162988c6843b88a3d5b6a225f
rameyyyy <147844357+rameyyyy@users.noreply.github.com>
2026-02-27 22:46:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-let index = 0;
-const slides = document.querySelector(".slides");
-const totalSlides = document.querySelectorAll(".card").length;
-
-setInterval(() => {
-  index++;
-  if (index >= totalSlides) {
-    index = 0;
-  }
-  slides.style.transform = `translateX(-${index * 100}%)`;
-}, 3000);
+document.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.style.background = window.scrollY > 50 
+    ? "rgba(0,0,0,0.8)" 
+    : "rgba(0,0,0,0.3)";
+});
